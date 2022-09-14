072255a28c3c1d2a55458b2910c44874025926ab
CoCo3-org <jross19@outlook.com>
2022-09-14 09:53:06 -0600

    fix _countLog

--- C#/UnitTests/SetUpFixture.cs
+++ C#/UnitTests/SetUpFixture.cs
@@ -12,7 +12,7 @@ namespace UnitTests
 		[OneTimeSetUp]
 		public void RunBeforeAnyTests()
 		{
-			StreamWriter sw = new StreamWriter(@"..\..\..\_countLog.txt", true);
+			StreamWriter sw = new StreamWriter(@"..\..\..\..\..\_countLog.txt", true);
 			sw.WriteLine("7422\tRetro.NET\t" + System.DateTime.Now + "\tUnit Tests");
 			sw.Close();
 		}
